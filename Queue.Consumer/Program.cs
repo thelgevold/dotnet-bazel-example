417f49d8f944cc1bbfdea9d5f296e3b958cfe093
Torgeir Helgevold <torgeirh@hotmail.com>
2019-08-11 11:07:01 -0400

    initial commit

---
+++ Queue.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+             
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "demo-greeting",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+
+            channel.BasicConsume(queue: "demo-greeting",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "demo-greeting",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                while (true)
+                {
+                    string message = $"Hello World! {DateTime.Now.ToString()}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                     routingKey: "demo-greeting",
+                                     basicProperties: null,
+                                     body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
